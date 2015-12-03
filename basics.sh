48365689a121633666b23cf509b24870b2d3154f
Yun Zhi Lin <yun@yunspace.com>
2015-12-03 23:01:36 +1100

    initial draft for brew installations

---
+++ basics.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# BREW with xcode
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+sudo xcodebuild
+brew update
+brew install caskroom/cask/brew-cask
+
+# BROWSERS
+brew cask install firefox google-chrome
+
+# JAVA
+brew cask install java
+echo 'export JAVA_HOME=$(/usr/libexec/java_home -v1.8)' >> ~/.bash_profile
+
+# NODE with chromedriver for web tests
+brew install nvm
+# Set up the working directory
+mkdir ~/.nvm
+# Set up paths, also add these to your .bash_profile
+echo 'export NVM_DIR=~/.nvm' >> ~/.bash_profile
+echo 'source $(brew --prefix nvm)/nvm.sh' >> ~/.bash_profile
+# install the desired version of NodeJS
+nvm install 5.0
+npm install -g bower chromedriver gulp
+
+# GRADLE, SCALA and other Java related stuff using sdkman
+curl -s http://get.sdkman.io | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install gradle
+echo 'org.gradle.daemon=true' >> ~/.gradle/gradle.properties
+sdk install scala
+sdk install sbt
+
+# GIT
+brew install git
+# OS X doesn't honour the path for git so we move the Apple version.
+sudo mv /usr/bin/git /usr/bin/git-apple
+# Generate SSH keys.
+ssh-keygen
+# Copy the SSH public key to the clip board, so you can put in github
+pbcopy < ~/.ssh/id_rsa.pub
+
+# IDEA
+brew cask install intellij-idea
---
+++ extras.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# RUBY
+brew install rbenv ruby-build
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+
+# DOCKER
+brew cask install virtualbox
+brew install docker docker-machine
+docker-machine create -d virtualbox local
+
+# DB
+brew install mongo
+brew install postgresql
